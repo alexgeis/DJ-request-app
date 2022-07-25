04d1b7695dd78d7d80d6b985d52293e7e1b7c926
Alex Geis <siegxela@gmail.com>
2022-07-25 15:19:15 -0600

    add example factory function

---
+++ src/js/create.js
@@ -0,0 +1,12 @@
+//EXAMPLE FACTORY FUNCTION
+
+// const createTask = ({ title, description, dueDate, priority, notes }) => ({
+// 	title,
+// 	description,
+// 	dueDate,
+// 	priority,
+// 	notes,
+// 	printTask() {
+// 		return `${this.title}: ${this.description}`;
+// 	},
+// });
