847cad32688ba92c218bbd3c4165432bfe04300c
Alex Geis <siegxela@gmail.com>
2022-08-01 08:05:02 -0600

    add eslint rules to resolve issues

--- src/js/create.js
+++ src/js/create.js
@@ -1,12 +1,12 @@
-//EXAMPLE FACTORY FUNCTION
+// EXAMPLE FACTORY FUNCTION
 
 // const createTask = ({ title, description, dueDate, priority, notes }) => ({
-// 	title,
-// 	description,
-// 	dueDate,
-// 	priority,
-// 	notes,
-// 	printTask() {
+// title,
+// description,
+// dueDate,
+// priority,
+// notes,
+// printTask() {
 // 		return `${this.title}: ${this.description}`;
 // 	},
 // });
