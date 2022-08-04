c551eb0b7a5743110bc1899d46c8744e239ff11a
Alex Geis <siegxela@gmail.com>
2022-08-04 07:53:03 -0600

    add nav dom creation with tabs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import "./css/style.scss";
+import { domCreate } from "./js/helpers/domCreate";
+
+const navEl = domCreate("nav", ["nav"]);
+const tabLink1 = domCreate("a", ["tab-link"], { id: "tab-link-1", href: "#" });
+const tabLink2 = domCreate("a", ["tab-link"], { id: "tab-link-2", href: "#" });
+const tabLink3 = domCreate("a", ["tab-link"], { id: "tab-link-3", href: "#" });
+const tabLink4 = domCreate("a", ["tab-link"], { id: "tab-link-4", href: "#" });
+navEl.append(tabLink1, tabLink2, tabLink3, tabLink4);
