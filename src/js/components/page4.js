794a4393e16eb4ad379ad75ab7d2047e7165819f
Alex Geis <siegxela@gmail.com>
2022-08-11 10:13:54 -0600

    fresh deploy

--- src/js/components/page4.js
+++ src/js/components/page4.js
@@ -6,7 +6,12 @@ function createPage4() {
 	header4.textContent = "Settings";
 	const text4 = domCreate("p");
 	text4.textContent = "This tab content is humble and doesn't have an ego";
-	page4.append(header4, text4);
+	const githubLink = domCreate("a", [], {
+		href: "https://github.com/alexgeis/DJ-request-app",
+		target: "_blank",
+	});
+	githubLink.textContent = "GITHUB REPO LINK";
+	page4.append(header4, text4, githubLink);
 	return page4;
 }
 
