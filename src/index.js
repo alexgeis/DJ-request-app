99801561915e1c63a5861bc4e3d3322e7d99c1e5
Alex Geis <siegxela@gmail.com>
2022-08-11 09:54:33 -0600

    modularize page 3 content and import into index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import domCreate from "./js/helpers/domCreate";
 import createNavMenu from "./js/components/navMenu";
 import createPage1 from "./js/components/page1";
 import createPage2 from "./js/components/page2";
+import createPage3 from "./js/components/page3";
 
 // nav menu
 const navMenu = createNavMenu();
@@ -11,13 +12,7 @@ const page1 = createPage1();
 // page 2
 const page2 = createPage2();
 // page 3
-const page3 = domCreate("div", ["tabcontent"], { id: "tab3" });
-const header3 = domCreate("h3");
-header3.textContent = "Tab 3";
-const text3 = domCreate("p");
-text3.textContent =
-	"Hard to understate the degree of awesome of this tab content";
-page3.append(header3, text3);
+const page3 = createPage3();
 // page 4
 const page4 = domCreate("div", ["tabcontent"], { id: "tab4" });
 const header4 = domCreate("h3");
---
+++ src/js/components/page3.js
@@ -0,0 +1,14 @@
+import domCreate from "../helpers/domCreate";
+
+function createPage3() {
+	const page3 = domCreate("div", ["tabcontent"], { id: "tab3" });
+	const header3 = domCreate("h3");
+	header3.textContent = "Tab 3";
+	const text3 = domCreate("p");
+	text3.textContent =
+		"Hard to understate the degree of awesome of this tab content";
+	page3.append(header3, text3);
+	return page3;
+}
+
+export default createPage3;
