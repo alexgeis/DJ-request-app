6839e88ecfd1a92a60648d17c891033f3a47ea22
Alex Geis <siegxela@gmail.com>
2022-09-06 09:38:54 -0600

    add all 4 status options to queue list items for reference examples

--- src/js/pages/page1.js
+++ src/js/pages/page1.js
@@ -1,5 +1,7 @@
 import domCreate from "../helpers/domCreate";
-import checkOutline from "../../assets/check-outline.png";
+import checkmark from "../../assets/check-bold.png";
+import rejectMark from "../../assets/close-octagon.png";
+import clock from "../../assets/clock-outline.png";
 
 function createPage1() {
 	const page1 = domCreate("div", ["tabcontent"], { id: "tab1" });
@@ -17,7 +19,7 @@ function createPage1() {
 
 	const statusImg1 = domCreate("img", [], {
 		id: "statusImg1",
-		src: checkOutline,
+		src: checkmark,
 	});
 	const statusDiv1 = domCreate("div", ["queue-status"], { id: "queueStatus1" });
 	statusDiv1.appendChild(statusImg1);
@@ -25,9 +27,30 @@ function createPage1() {
 
 	const listEl2 = domCreate("li", ["queue-list-item"], { id: "list-item-2" });
 	listEl2.textContent = "Song title / Artist";
+
+	const statusImg2 = domCreate("img", [], {
+		id: "statusImg2",
+		src: rejectMark,
+	});
+	const statusDiv2 = domCreate("div", ["queue-status"], { id: "queueStatus2" });
+	statusDiv2.appendChild(statusImg2);
+	listEl2.appendChild(statusDiv2);
+
 	const listEl3 = domCreate("li", ["queue-list-item"], { id: "list-item-3" });
 	listEl3.textContent = "Song title / Artist";
-	ULel.append(listEl1, listEl2, listEl3);
+
+	const statusImg3 = domCreate("img", [], {
+		id: "statusImg3",
+		src: clock,
+	});
+	const statusDiv3 = domCreate("div", ["queue-status"], { id: "queueStatus3" });
+	statusDiv3.appendChild(statusImg3);
+	listEl3.appendChild(statusDiv3);
+
+	const listEl4 = domCreate("li", ["queue-list-item"], { id: "list-item-4" });
+	listEl4.textContent = "Song title / Artist";
+
+	ULel.append(listEl1, listEl2, listEl3, listEl4);
 	queueDiv.appendChild(ULel);
 
 	page1.append(header1, text1, queueDiv);
