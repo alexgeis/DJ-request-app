1cd63d59414243be0dd78ac8fad96e2947d6fdce
Alex Geis <siegxela@gmail.com>
2022-09-12 17:17:10 -0600

    replace github logo w/ link

--- src/js/pages/page4.js
+++ src/js/pages/page4.js
@@ -1,18 +1,27 @@
 import domCreate from "../helpers/domCreate";
+import githubLogo from "../../assets/GitHub-Mark-64px.png";
 
 function createPage4() {
 	const page4 = domCreate("div", ["tabcontent"], { id: "tab4" });
 	const header4 = domCreate("h3", ["tab-header"]);
 	header4.textContent = "Settings";
+
 	const text4 = domCreate("p", ["tab-sub-header"]);
 	text4.textContent = "This tab content is humble and doesn't have an ego";
+
 	const githubLink = domCreate("a", ["link-github"], {
 		href: "https://github.com/alexgeis/DJ-request-app",
 		target: "_blank",
 	});
-	githubLink.textContent = "GITHUB REPO LINK";
+	const gitLogoEl = domCreate("img", ["github-logo"], {
+		id: "githubLogo",
+		src: githubLogo,
+	});
+	githubLink.appendChild(gitLogoEl);
+
 	const githubLinkContainer = domCreate("div", [], { id: "githubContainer" });
 	githubLinkContainer.appendChild(githubLink);
+
 	page4.append(header4, text4, githubLinkContainer);
 	return page4;
 }
